f5aec43713f75f12774b7bb23253cc4606fc0fda
yarungrung <s1243032@gm.ncue.edu.tw>
2025-05-21 15:18:53 +0800

    Update 3_interactive.py

--- pages/3_interactive.py
+++ pages/3_interactive.py
@@ -1,1 +1,35 @@
+import streamlit as st
+import leafmap.foliumap as leafmap
 
+markdown = """
+Department of Geography,
+
+National Changhua University of Education
+
+<https://geo3w.ncue.edu.tw/?Lang=zh-tw>
+"""
+
+st.sidebar.title("About")
+st.sidebar.info(markdown)
+logo = "https://i.imgur.com/UbOXYAU.png"
+st.sidebar.image(logo)
+
+
+st.title("Interactive Map")
+
+col1, col2 = st.columns([4, 1])
+options = list(leafmap.basemaps.keys())
+index = options.index("OpenTopoMap")
+
+with col2:
+
+    basemap = st.selectbox("Select a basemap:", options, index)
+
+
+with col1:
+
+    m = leafmap.Map(
+        locate_control=True, latlon_control=True, draw_export=True, minimap_control=True
+    )
+    m.add_basemap(basemap)
+    m.to_streamlit(height=700)
