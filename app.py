eb5cdc55fe04dfb4e85a25ccb0991ac662a52052
yarungrung <s1243032@gm.ncue.edu.tw>
2025-05-21 14:36:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,38 +1,62 @@
 import streamlit as st
-import ee
-from google.oauth2 import service_account
-import geemap.foliumap as geemap
+from datetime import date
 
-# 從 Streamlit Secrets 讀取 GEE 服務帳戶金鑰 JSON
-service_account_info = st.secrets["GEE_SERVICE_ACCOUNT"]
+st.set_page_config(layout="wide", page_title="這是Streamlit App第二次練習！")
 
-# 使用 google-auth 進行 GEE 授權
-credentials = service_account.Credentials.from_service_account_info(
-    service_account_info,
-    scopes=["https://www.googleapis.com/auth/earthengine"]
+st.title("應用程式主頁")
+
+st.markdown(
+    """
+    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io), [GEE](https://earthengine.google.com/), 
+    [geemap](https://leafmap.org) and [leafmap](https://leafmap.org). 
+    """
 )
 
-# 初始化 GEE
-ee.Initialize(credentials)
+st.header("Instructions")
+
+markdown = """
+1. You can use it as a template for your own project.
+2. Customize the sidebar by changing the sidebar text and logo in each Python file.
+3. Find your favorite emoji from https://emojipedia.org.
+4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
+
+"""
+
+st.markdown(markdown)
 
 
-###############################################
-st.set_page_config(layout="wide")
-st.title("🌍 使用服務帳戶連接 GEE 的 Streamlit App")
 
 
-# 地理區域
-point = ee.Geometry.Point([121.56, 25.03])
+st.title("選擇日期區間")
 
-# 擷取 Landsat NDVI
-image = ee.ImageCollection("LANDSAT/LC09/C02/T1_L2") \
-    .filterBounds(point) \
-    .filterDate("2022-01-01", "2022-12-31") \
-    .median()
 
-ndvi = image.normalizedDifference(["SR_B5", "SR_B4"]).rename("NDVI")
+# 初始化 session_state
+#if 'start_date' not in st.session_state:
+#    st.session_state['start_date'] = date(2024, 1, 1)
+#if 'end_date' not in st.session_state:
+#    st.session_state['end_date'] = date.today()
 
-# 顯示地圖
-Map = geemap.Map(center=[25.03, 121.56], zoom=10)
-Map.addLayer(ndvi, {"min": 0, "max": 1, "palette": ["white", "green"]}, "NDVI")
-Map.to_streamlit(height=600)
+st.session_state['start_date'] = date(2024, 1, 1)
+st.session_state['end_date'] = date.today()
+
+
+# 日期選擇器
+start_date = st.date_input(label = "選擇起始日期", value = st.session_state['start_date'], min_value = date(2018, 1, 1), max_value = date.today())
+end_date = st.date_input(label = "選擇結束日期", value = st.session_state['end_date'], min_value = start_date, max_value = date.today())
+
+# 儲存使用者選擇
+st.session_state['start_date'] = start_date
+st.session_state['end_date'] = end_date
+
+st.success(f"目前選擇的日期區間為：{start_date} 到 {end_date}")
+
+
+st.title("利用擴充器示範")
+
+with st.expander("展示gif檔"):
+    st.image("pucallpa.gif")
+
+with st.expander("播放mp4檔"):
+    video_file = open("pucallpa.mp4", "rb")  # "rb"指的是讀取二進位檔案（圖片、影片）
+    video_bytes = video_file.read()
+    st.video(video_bytes)
